22872ea8b519b1f10f482b070ea807b44ed50e2f
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-11-09 20:26:03 -0300

    feat(Enum): add Brazilian states to Estado enum  - Added all 27 Brazilian states and the Federal District to the Estado enum.

--- src/Enum/Estado.php
+++ src/Enum/Estado.php
@@ -2,4 +2,33 @@
 
 namespace AdaiasMagdiel\G1\Enum;
 
-enum Estado: string {}
+enum Estado: string
+{
+	case ACRE = 'ac/acre';
+	case ALAGOAS = 'al/alagoas';
+	case AMAPA = 'ap/amapa';
+	case AMAZONAS = 'am/amazonas';
+	case BAHIA = 'ba/bahia';
+	case CEARA = 'ce/ceara';
+	case DISTRITO_FEDERAL = 'df/distrito-federal';
+	case ESPIRITO_SANTO = 'es/espirito-santo';
+	case GOIAS = 'go/goias';
+	case MARANHAO = 'ma/maranhao';
+	case MATO_GROSSO = 'mt/mato-grosso';
+	case MATO_GROSSO_DO_SUL = 'ms/mato-grosso-do-sul';
+	case MINAS_GERAIS = 'mg/minas-gerais';
+	case PARA = 'pa/para';
+	case PARAIBA = 'pb/paraiba';
+	case PARANA = 'pr/parana';
+	case PERNAMBUCO = 'pe/pernambuco';
+	case PIAUI = 'pi/piaui';
+	case RIO_DE_JANEIRO = 'rj/rio-de-janeiro';
+	case RIO_GRANDE_DO_NORTE = 'rn/rio-grande-do-norte';
+	case RIO_GRANDE_DO_SUL = 'rs/rio-grande-do-sul';
+	case RONDONIA = 'ro/rondonia';
+	case RORAIMA = 'rr/roraima';
+	case SANTA_CATARINA = 'sc/santa-catarina';
+	case SAO_PAULO = 'sp/sao-paulo';
+	case SERGIPE = 'se/sergipe';
+	case TOCANTINS = 'to/tocantins';
+}
