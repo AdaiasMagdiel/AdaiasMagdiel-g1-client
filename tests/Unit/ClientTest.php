9d525cf0ff0eea1fd5c739b3ab6426881c64c200
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-11-12 11:52:29 -0300

    tests: Add caching to ClientTest  - Added caching directory constant CACHE_DIR  - Updated Client instantiation to use CACHE_DIR  - Added afterAll hook to remove caching directory after tests  - Updated test cases to utilize caching

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -4,8 +4,30 @@ use AdaiasMagdiel\G1\Client;
 use AdaiasMagdiel\G1\Enum\Estado;
 use AdaiasMagdiel\G1\Response\Ultimas;
 
+const CACHE_DIR = __DIR__ . "/.g1-cache-test";
+
+afterAll(function () {
+    function removeDir($dir)
+    {
+        $arquivos = scandir($dir);
+        foreach ($arquivos as $arquivo) {
+            if ($arquivo != '.' && $arquivo != '..') {
+                $caminho = $dir . '/' . $arquivo;
+                if (is_file($caminho)) {
+                    unlink($caminho);
+                } elseif (is_dir($caminho)) {
+                    removeDir($caminho);
+                }
+            }
+        }
+        rmdir($dir);
+    }
+
+    removeDir(CACHE_DIR);
+});
+
 test('fetches latest news without state', function () {
-    $client = new Client();
+    $client = new Client(cacheDir: CACHE_DIR);
     $ultimas = $client->ultimas();
 
     expect($ultimas)->toBeInstanceOf(Ultimas::class);
@@ -17,7 +39,7 @@ test('fetches latest news without state', function () {
 });
 
 test('fetches latest news with a specific state', function () {
-    $client = new Client();
+    $client = new Client(cacheDir: CACHE_DIR);
     $ultimas = $client->ultimas(estado: Estado::PARA);
 
     expect($ultimas)->toBeInstanceOf(Ultimas::class);
@@ -29,7 +51,7 @@ test('fetches latest news with a specific state', function () {
 });
 
 test('fetches latest news with specific page', function () {
-    $client = new Client();
+    $client = new Client(cacheDir: CACHE_DIR);
     $ultimas = $client->ultimas(page: 5);
 
     expect($ultimas)->toBeInstanceOf(Ultimas::class);
@@ -41,7 +63,7 @@ test('fetches latest news with specific page', function () {
 });
 
 test('fetches latest news with specific page and specific state', function () {
-    $client = new Client();
+    $client = new Client(cacheDir: CACHE_DIR);
     $ultimas = $client->ultimas(page: 10, estado: Estado::ESPIRITO_SANTO);
 
     expect($ultimas)->toBeInstanceOf(Ultimas::class);
